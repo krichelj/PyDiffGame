0822064dd50d0ba3a8a01b908e093c9d90527094
shaykrichelifrenn <skricheli2@gmail.com>
2022-06-29 19:11:27 +0300

    updated theory in MassesWithSprings.py

--- src/PyDiffGame/examples/MassesWithSprings.py
+++ src/PyDiffGame/examples/MassesWithSprings.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.linalg import inv
 from PyDiffGame.PyDiffGame import PyDiffGame
 
 from PyDiffGame.ContinuousPyDiffGame import ContinuousPyDiffGame
@@ -8,7 +9,7 @@ class MassesWithSprings(ContinuousPyDiffGame):
     def __init__(self,
                  m: float,
                  k: float,
-                 q_x: float,
+                 q: float,
                  r: float,
                  x_0: np.array = None,
                  x_T: np.array = None,
@@ -25,25 +26,30 @@ class MassesWithSprings(ContinuousPyDiffGame):
                       [-2 * self.__k / self.__m, self.__k / self.__m, 0, 0],
                       [self.__k / self.__m, -2 * self.__k / self.__m, 0, 0]])
 
-        B_1 = np.array([[0],
-                        [0],
-                        [1 / 2],
-                        [1 / 2]])
-        B_2 = np.array([[0],
-                        [0],
-                        [1 / 2],
-                        [-1 / 2]])
+        B = 1 / self.__m * np.array([[0, 0],
+                                     [0, 0],
+                                     [1, 0],
+                                     [0, 1]])
 
-        B_lqr = 1 / self.__m * np.array([[0, 0],
-                                         [0, 0],
-                                         [1, 0],
-                                         [0, 1]])
+        M_1 = 1 / (2 * self.__m) * np.array([[1, 1]])
+        M_2 = 1 / (2 * self.__m) * np.array([[1, -1]])
+        m_1 = M_1.shape[0]
+        m_2 = M_2.shape[0]
+
+        M = np.concatenate([M_1, M_2])
+        M_inv = inv(M)
+        M_1_tilde = M_inv[:, 0:m_1]
+        M_2_tilde = M_inv[:, m_1:m_1 + m_2]
+
+        n = A.shape[0]
+        B_1 = (B @ M_1_tilde).reshape((n, m_1))
+        B_2 = (B @ M_2_tilde).reshape((n, m_2))
+
         B_multiplayer = [B_1, B_2]
 
-        q_v = q_x ** 2
-        Q_1 = np.diag([q_x, 0, q_v, q_v])
-        Q_2 = np.diag([0, q_x, q_v, q_v])
-        Q_lqr = np.diag([q_x, q_x, q_v, q_v])
+        Q_1 = np.diag([q, 0, q ** 2, q ** 2])
+        Q_2 = np.diag([0, q, q ** 2, q ** 2])
+        Q_lqr = np.diag([q, q, q ** 2, q ** 2])
         Q_multiplayer = [Q_1, Q_2]
         R_lqr = np.diag([r, r])
         R_multiplayer = [np.array([r])] * 2
@@ -53,7 +59,7 @@ class MassesWithSprings(ContinuousPyDiffGame):
         state_variables_names = ['x_1', 'x_2', '\\dot{x}_1', '\\dot{x}_2']
 
         super().__init__(A=A,
-                         B=B_lqr if regular_LQR else B_multiplayer,
+                         B=B if regular_LQR else B_multiplayer,
                          Q=Q_lqr if regular_LQR else Q_multiplayer,
                          R=R_lqr if regular_LQR else R_multiplayer,
                          x_0=x_0,
@@ -62,12 +68,11 @@ class MassesWithSprings(ContinuousPyDiffGame):
                          state_variables_names=state_variables_names,
                          epsilon=epsilon,
                          L=L,
-                         force_finite_horizon=T_f is not None
-                         )
+                         force_finite_horizon=T_f is not None)
 
 
-m, k = 10, 1000
-q_x, r = 1000, 10000
+m, k = 10, 100
+q, r = 1000, 10000
 
 x_0 = np.array([1,  # x_1
                 2,  # x_2
@@ -84,24 +89,25 @@ epsilon = 10 ** (-3)
 
 lqr_masses = MassesWithSprings(m=m,
                                k=k,
-                               q_x=q_x,
+                               q=q,
                                r=r,
                                x_0=x_0,
                                x_T=x_T,
                                regular_LQR=True,
                                epsilon=epsilon
                                )
-lqr_masses.run_simulation()
-lqr_masses.save_figure()
+lqr_masses.run_simulation(plot_state_space=True)
+# lqr_masses.save_figure(filename='LQR')
 
 game_masses = MassesWithSprings(m=m,
                                 k=k,
-                                q_x=q_x,
+                                q=q,
                                 r=r,
                                 x_0=x_0,
                                 x_T=x_T,
                                 epsilon=epsilon
                                 )
-game_masses.run_simulation()
+game_masses.run_simulation(plot_state_space=True)
+# game_masses.save_figure(filename='game')
 
-# print(f'LQR cost: {lqr_masses.get_costs() - game_masses.get_costs().max()}')
+# print(f'cost difference: {lqr_masses.get_costs() - game_masses.get_costs().max()}')
